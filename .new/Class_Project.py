bdf142f633a00a3b5d4ec6b4e4ab6e48d2c0b3eb
Mara Joyce <mara.craig1994@gmail.com>
2020-07-26 19:57:17 -0500

    Class Project Draft

---
+++ .new/Class_Project.py
@@ -0,0 +1,22 @@
+while True:
+    input ("Eneter Zip Code")
+    Zip_code= int 
+    import http://openweathermap.org/ 
+    #getting weather forcast from web page
+    connection = import http://openweathermap.org/
+def connectionSetting()
+    if connection = failed
+	pring("Connection failed please try again")
+    else connection= success
+	Print("Gathering requested information")
+
+while Zip_code in http://openweathermap.org/
+#Getting the zip_code put into the webpage to get forcast 
+weather = int http://openweathermap.org/
+while True:
+    connectionSetting(Zip_code) 
+    print("Your weather forecast is", weather)
+    if connection= failed
+        print("Zip_code not found, please try again")
+	break
+
